01845085658daec3b340a61a4a663b1e261e3cb4
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-11-22 23:43:21 +0900

    テンプレートに値を設定するサンプル

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ server.listen(3000);
 console.log('Server start!');
 
 function getFromClient(request, response) {
-    let content = ejs.render(index_page);
+    let content = ejs.render(index_page, {
+        title: "Indexページ",
+        content: "これはテンプレートを使ったサンプルページです。",
+    });
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(content);
     response.end();
