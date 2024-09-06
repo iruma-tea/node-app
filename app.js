1d2294108e075b95c1faf58e9586f764f1871c6a
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-09-07 08:08:57 +0900

    コメントを追加

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import http from "http"
 
+// リスト2-1 (start)
 let server = http.createServer(
     (request, response) => {
         response.end('Hello Node.js');
     }
 );
-
 server.listen(3000);
+// リスト2-1 (end)
