ca23166bc7123c9ff785301981f1c31a0631e73b
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-09-07 08:16:18 +0900

    フォーマッタを変更

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-import http from "http"
+import http from "http";
 
 // リスト2-1 (start)
-let server = http.createServer(
-    (request, response) => {
-        response.end('Hello Node.js');
-    }
-);
+let server = http.createServer((request, response) => {
+  response.end("Hello Node.js");
+});
 server.listen(3000);
 // リスト2-1 (end)
