dab54e315e8b10d13cc6916a691510d8e960b725
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-09-07 09:00:50 +0900

    リスト2-2に変更

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import http from "http";
 
 // リスト2-1 (start)
-let server = http.createServer((request, response) => {
-  response.end("Hello Node.js");
-});
-server.listen(3000);
+// let server = http.createServer((request, response) => {
+//   response.end("Hello Node.js");
+// });
 // リスト2-1 (end)
+
+// リスト2-2(start)
+let server = http.createServer((request, response) => {
+  response.end(
+    "<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>"
+  );
+});
+// リスト2-2(end)
+server.listen(3000);
