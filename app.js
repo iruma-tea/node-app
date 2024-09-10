01ac80998da69750ca4f543393c7ed32a9ff816c
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-09-10 11:59:18 +0900

    リスト2-3まで学習完了

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 import http from "http";
 
 // リスト2-1 (start)
-// let server = http.createServer((request, response) => {
+// const server = http.createServer((request, response) => {
 //   response.end("Hello Node.js");
 // });
 // リスト2-1 (end)
 
 // リスト2-2(start)
-let server = http.createServer((request, response) => {
-  response.end(
-    "<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>"
-  );
-});
+// const server = http.createServer((request, response) => {
+//   response.end(
+//     "<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>"
+//   );
+// });
 // リスト2-2(end)
+
+// リスト2-3(start)
+const server = http.createServer((request, response) => {
+  response.setHeader("Content-Type", "text/html");
+  response.write('<!DOCTYPE html><html lang="ja">');
+  response.write('<head><meta charset="utf-8">');
+  response.write("<title>Hello</title></head>");
+  response.write("<body><h1>Hello Node.js!</h1>");
+  response.write("<p>This is Node.js sample.page</p>");
+  response.write("<p>これは、Node.jsのサンプルページです。</p>", "utf8");
+  response.write("</body></html>");
+  response.end();
+});
+// リスト2-3(end)
 server.listen(3000);
