f281c344caa4524bc9adf392ffe58e5c6d5b8833
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-09-10 16:53:11 +0900

    リスト2-8 ejsのサンプルを追加

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import * as http from "http";
 
 // リスト2-5(start)
-// リスト2-5(end)
 import * as fs from "fs";
+// リスト2-5(end)
+// リスト2-8(start)
+import * as ejs from "ejs";
+// リスト2-8(end)
 
 // リスト2-1 (start)
 // const server = http.createServer((request, response) => {
@@ -42,6 +45,10 @@ import * as fs from "fs";
 // });
 // リスト2-5(end)
 
+// リスト2-8(start)
+const index_page = fs.readFileSync("./index.ejs", "utf-8");
+// リスト2-8(end)
+
 // リスト2-6(start)
 const server = http.createServer(getFromClient);
 // リスト2-6(end)
@@ -50,13 +57,22 @@ server.listen(3000);
 console.log("Server start!");
 
 // createServerの処理
-function getFromClient(req, res) {
-  let request = req;
-  let response = res;
+// function getFromClient(req, res) {
+//   let request = req;
+//   let response = res;
 
-  fs.readFile("./index.html", "UTF-8", (error, data) => {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(data);
-    response.end();
-  });
+//   fs.readFile("./index.html", "UTF-8", (error, data) => {
+//     response.writeHead(200, { "Content-Type": "text/html" });
+//     response.write(data);
+//     response.end();
+//   });
+// }
+
+// リスト2-8(start)
+function getFromClient(request, response) {
+  let content = ejs.render(index_page);
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(content);
+  response.end();
 }
+// リスト2-8(end)
